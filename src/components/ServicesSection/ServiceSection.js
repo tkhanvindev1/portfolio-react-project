b9011ed31fd2622c609d38eb88653ec9178086a7
Tkhan Vin <gatorfl2014@gmail.com>
2022-08-30 16:33:26 -0400

    add technologies section

--- src/components/ServicesSection/ServiceSection.js
+++ src/components/ServicesSection/ServiceSection.js
@@ -1,27 +1,30 @@
 import React from "react";
 import "/Users/vinhnguyen/Documents/DevMountain/f25/spec/Week 3/css-basic-clone/portfolio/src/components/ServicesSection/ServicesSection.css";
 const services = [
-  { id: 1, img: "https://icons-for-free.com/download-icon-facebook+logo+logo+website+icon-1320190502625926346_256.ico", label: "dsads", text: "asdasd" },
-  { id: 2, img: "https://icons-for-free.com/download-icon-facebook+logo+logo+website+icon-1320190502625926346_256.ico", label: "asdasd", text: "asd" },
-  { id: 3, img: "https://icons-for-free.com/download-icon-facebook+logo+logo+website+icon-1320190502625926346_256.ico", label: "asdasd", text: "asdasd" },
+  { id: 1, img: 1, label: "dsads", text: "asdasd" },
+  { id: 2, img: 2, label: "asdasd", text: "asd" },
+  { id: 3, img: 3, label: "asdasd", text: "asdasd" },
+  { id: 4, img: 4, label: "asdasd", text: "asdasd" },
+  { id: 5, img: 5, label: "asdasd", text: "asdasd" },
+  { id: 6, img: 6, label: "asdasd", text: "asdasd" },
+  { id: 7, img: 7, label: "asdasd", text: "asdasd" },
+  { id: 8, img: 8, label: "asdasd", text: "asdasd" },
 ];
 
 const ServiceSection = () => {
   return (
     <div className="services-container">
       <div className="text-center">
-        <h2 data-aos="fade-up">SERVICES</h2>
-        <h3 data-aos="fade-left">Lorem ipsum dolor sit amet consectetur.</h3>
+        <h2 data-aos="fade-up">TECHNOLOGIES</h2>
+        <h3 data-aos="fade-left">developed profesional skills with</h3>
       </div>
       <div className="services-list">
         <div className="service row">
           {services.map(function (service) {
-            const { id, img, label, text } = service;
+            const { id, img } = service;
             return (
               <div className='service' key={`${id}`}>
-                <img src={`${img}`} />
-                <h2>{`${label}`} </h2>
-                <p>{`${text}`}</p>
+                <img className="icons-container" src={require(`/Users/vinhnguyen/Documents/DevMountain/f25/spec/Week 3/css-basic-clone/portfolio/src/img/tech-icons/` + `${img}` + `.png`)} />
               </div>
             );
           })}
